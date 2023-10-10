0874ba6149cd78b21ff10cfa7a293d57b86ff3d4
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2023-10-10 15:14:21 +0700

    Updated rust to 1.73.0

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --progress=plain -t vegansk/sbt-node-rust:11.0.10_1.4.9_16.20.2_1.67.1 -f ./docker/Dockerfile .
+docker build --progress=plain -t vegansk/sbt-node-rust:11.0.10_1.4.9_16.20.2_1.73.0 -f ./docker/Dockerfile .
